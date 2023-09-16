74c4a47e48d4fa8c595803e5f644de2b7202c79d
Farouk Adegboyega <faroukadekunle001@gmail.com>
2023-09-16 01:48:56 +0100

    🦋: God! I hate deployments.

--- anima_lib_team_91/wsgi.py
+++ anima_lib_team_91/wsgi.py
@@ -14,3 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'anima_lib_team_91.settings')
 
 application = get_wsgi_application()
+app = application
