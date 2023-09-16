93a9e2354c5d35c4ecbf7fa379335d2cfd8d942f
Farouk Adegboyega <faroukadekunle001@gmail.com>
2023-09-16 05:16:25 +0100

    😒: taking another run at deployment

--- anima_lib_team_91/settings.py
+++ anima_lib_team_91/settings.py
@@ -158,7 +158,6 @@ STATICFILES_DIRS = (
 )
 
 
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
--- anima_lib_team_91/urls.py
+++ anima_lib_team_91/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),                            # admin
     path('', include('home.urls')),                             # base app for the website
